bae8bc8a13702324c487250400741f55469e15f0
ShayneGeo <92595748+ShayneGeo@users.noreply.github.com>
2025-05-07 12:55:47 -0600

    Update appNET.py

--- appNET.py
+++ appNET.py
@@ -794,8 +794,8 @@ if st.button("Run Simulation"):
         st.pyplot(fig)
         st.success("Simulation complete!")
 
-    finally:
-        for p in [slope_path, fuel_path]:
-            try: os.remove(p)
-            except: pass
+    # finally:
+    #     for p in [slope_path, fuel_path]:
+    #         try: os.remove(p)
+    #         except: pass
 
